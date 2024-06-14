fc9b5a0c76b2662d99cce17814fd2d8d72acc22e
mayochankawaii <171436876+mayochankawaii@users.noreply.github.com>
2024-06-14 07:34:10 +0000

    bookers Commit

---
+++ app/controllers/books_controller.rb
@@ -0,0 +1,21 @@
+class BooksController < ApplicationController
+
+  def new
+    @book = Book.new
+  end
+
+  def create
+    book = Book.new(book_params)
+    book.save
+    redirect_to 'books/show'
+  end
+
+  def index
+  end
+
+  def show
+  end
+
+  def edit
+  end
+end
