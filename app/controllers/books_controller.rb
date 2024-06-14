bf4e5591bd2ffbdc7fa1497b3639cf6b13a42c98
mayochankawaii <171436876+mayochankawaii@users.noreply.github.com>
2024-06-14 16:47:49 +0000

    [start] bookのindexページの作成

--- app/controllers/books_controller.rb
+++ app/controllers/books_controller.rb
@@ -7,13 +7,16 @@ class BooksController < ApplicationController
   def create
     book = Book.new(book_params)
     book.save
-    redirect_to 'books/show'
+    redirect_to '/books/show'
   end
 
   def index
+    @books = Book.all
+    @book = Book.new
   end
 
   def show
+    @book = Book.find(params[:id])
   end
 
   def edit
